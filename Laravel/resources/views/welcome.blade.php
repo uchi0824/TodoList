8efd413cf023c1560e91f092017ea1008cc7bcad
uchi0824 <riversidetown@icloud.com>
2020-07-11 19:13:17 +0900

    change uchimura

--- Laravel/resources/views/welcome.blade.php
+++ Laravel/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Uchimura
                 </div>
 
                 <div class="links">
