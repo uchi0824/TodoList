d087826711f63e110b7085657829c88dadeb4dcf
uchi0824 <riversidetown@icloud.com>
2020-07-31 07:49:52 +0900

    修正

--- Laravel/resources/js/components/Todo.js
+++ Laravel/resources/js/components/Todo.js
@@ -1,42 +1,44 @@
-import React, { Component } from "react";
-import ReactDOM from "react-dom";
+import React,{useState,useEffect} from 'react';
+import ReactDOM from 'react-dom';
 import axios from "axios";
-import Button from '@material-ui/core/Button';
+import {TestButton as Btna} from './testbutton';
 
-export default class Todo extends Component {
-    constructor() {
-        super();
-        this.state = {
-            todo: []
+export default function Todo() {
+    const [todo,setTodo] = useState([]);
+
+    // axios
+    //     .get('/api/todo')
+    //     .then(response => {
+    //         setTodo(response.data);
+
+
+    //     }).catch(error => {
+    //         console.log(error);
+    //     });
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const res = await axios.get('/api/todo');
+            setTodo(res.data);
         };
-    }
-    componentDidMount() {
-        axios
-            .get("/api/todo")
-            .then(response => {
-                this.setState({
-                    todo: response.data
-                });
-            })
-            .catch(error => {
-                console.log(error);
-            });
-    }
-    render() {
-        const list = this.state.todo.map(item => {
-            return <li key={item.id}>{item.title}</li>;
-        });
-        return (
-            <div>
-                <ul className="todo-list">{list}</ul>
-                <Button variant="contained" color="primary">
-                    Hello World
-                </Button>
-            </div>
-        );
-    }
+
+        fetchData();
+      }, []);
+
+    return (
+        <div>fuga
+            <ul className="todo-list">
+                {todo.map((item) => {
+                    return <li key={item.id}>{item.title}</li>;
+                })}
+            </ul>
+            <Btna text="uchi"></Btna>
+            <Btna text="yoshi"></Btna>
+        </div>
+    );
 }
 
-if (document.getElementById("example")) {
-    ReactDOM.render(<Todo />, document.getElementById("example"));
+
+if (document.getElementById('example')) {
+    ReactDOM.render(<Todo />, document.getElementById('example'));
 }
